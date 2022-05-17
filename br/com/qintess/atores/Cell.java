57637fcaaefbc052086570e411db5b52b00834e9
FernandoFrancaDev <104387118+FernandoFrancaDev@users.noreply.github.com>
2022-05-17 16:26:46 -0300

    Criado a base do game

--- br/com/qintess/atores/Cell.java
+++ br/com/qintess/atores/Cell.java
@@ -5,7 +5,11 @@ public class Cell  {
 	private int linha;
 	private int coluna;
 	
-	public void Cell(int linha, int coluna) {
+	public Cell() {
+		
+	}
+	
+	public Cell(int linha, int coluna) {
 		
 		this.linha = linha;
 		this.coluna = coluna;
